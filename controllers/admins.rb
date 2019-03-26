006a36654d47d98a1ad10679ca81477c7c2d2f29
user <user@users-MacBook-Pro-5.local>
2019-03-26 11:54:30 +0000

    controllers and views set up

---
+++ controllers/admins.rb
@@ -0,0 +1,20 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( '../models/content.rb' )
+require_relative( '../models/rentals.rb' )
+require_relative( '../models/user.rb' )
+also_reload( '../models/*' )
+
+get '/admin' do
+  erb(:"admin/adminsearch")
+end
+
+get '/admin/users' do
+  @users = User.all
+  erb(:"admin/allusers")
+end
+
+get '/admin/rentals' do
+  erb
+end
